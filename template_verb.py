c629aeea20b8e314670803477fe3b8eed2cef786
Kabit Tarah <Kabit@example.com>
2021-01-03 15:21:16 -0500

    updates for special cases in 'treffen'

--- template_verb.py
+++ template_verb.py
@@ -3,24 +3,31 @@ import requests
 import jinja2
 import sys
 import re
+import unicodedata
+
+def sanitize_text(txt) -> str:
+    # Some web data contains zero width strings and possibly other control characters. This strips them out
+    return ''.join([c for c in txt if unicodedata.category(c)[0]!="C"])
+
+# Constants
+oauth_file = "/home/pi/.secrets/wp_oauth.json"
+site_file = "/home/pi/.secrets/wp_site.json"
 
 # Arguments:
 #   $1 german verb
-
 if len(sys.argv) == 1:
     print("No arguments given. Please supply a verb (Deutsch)")
     exit()
 
+# All CLI arguments make up the total verb
 verb = ' '.join(sys.argv[1:])
+# set this in the jinja replacement dictionary
 t_vars = {"verb_de": verb}
 print(f"German verb: { verb }")
 
-oauth_file = "/home/pi/.secrets/wp_oauth.json"
-site_file = "/home/pi/.secrets/wp_site.json"
-
+# Get OAUTH info from WP.com
 payload = json.loads(open(oauth_file).read())
 site_info = json.loads(open(site_file).read())
-
 site = site_info["site"]
 template_id = site_info["template"]
 
@@ -30,106 +37,151 @@ api_url = "https://public-api.wordpress.com/rest/v1.1"
 payload["grant_type"] = "password"
 oauth_req = requests.post(oauth_url, data=payload)
 oauth_resp = json.loads(oauth_req.text)
-header = {"Authorization": "Bearer " + oauth_resp["access_token"]}
+auth_header = {"Authorization": "Bearer " + oauth_resp["access_token"]}
 
+# Get the template post (usually private). This post should be in Jinja2 formatted HTML
 api = "/sites/" + site + "/posts/" + template_id
 url = api_url + api
-
-wp_req = requests.get(url, headers=header)
+wp_req = requests.get(url, headers=auth_header)
 wp_resp = json.loads(wp_req.text)
-
+# This sets up the template object within the jinja2 framework
 template = jinja2.Environment(loader=jinja2.BaseLoader).from_string(wp_resp["content"])
 
 # Get HTML search page for verb requested
-verb_search = requests.get("https://dict.leo.org/german-english/" + verb).text
+verb_search = sanitize_text(requests.get("https://dict.leo.org/german-english/" + verb).text)
 
-# Goals:
-# 1. Find 1st entry (table row <tr ...>) for the verb requested
-# --- <tr data-dz-ui="dictentry"...>...data-dz-flex-label-1="<verb>( \([0-9]+\))?"...</tr>
-#
-# 2. Find table cell with conj table link
-#
-# 3. Find table cell with info link
-#
-# 4. Find english translation
+# General steps below:
+# A. Get sections. These are identified by <thead> (separate tables on dict.leo)
+# B. From the Verbs section, get all rows
+# C. Step through each row to get the info we need. Ask the user at each step
+#    User input is needed because multiple english translations exist for some verbs and different conjugation tables exist
+#    (though present tense is the same on all seen so far)
+
+# A. Get table sections (Nouns, Verbs, etc) and key by section title
+section_dic = {}
+re_sec = re.compile(f'(<thead>(((?!<thead>).)*))')
+re_row = re.compile(f'(<tr>(((?!</tr>).)*)</tr>)')
+sections = re_sec.findall(verb_search)
+for section in sections:
+    header = re_row.search(section[0])
+    if header:
+        title = re.search(r'<h2 (((?!>).)*)>(((?!<).)*)</h2>', header.group())
+        # 0 = rest of header
+        # 1 = last char of 0
+        # 2 = title
+        # 3 = last char of title
+        if title:
+            section_dic[title.groups()[2]] = section[0]
+if 'Verbs' not in section_dic.keys():
+    print(f"No verb section for verb { verb }!")
+    exit()
 
 # Special Cases:
-# gehen  - "normal" case (completed)
-# kommen - two table sets have labels "kommen (1)" and "kommen (2)" (completed)
-# tun    -- special case in conj table - no root (all is conj extension and is in pink. Change + to * for root part)
+# gehen   - "normal" case (completed)
+# kommen  - two table sets have labels "kommen (1)" and "kommen (2)" (completed)
+# tun     - special case in conj table - no root (all is conj extension and is in pink. Change + to * for root part)
+# treffen - Nouns come first (split up sections and grabbed the Verb section - some reusable code here if I move on to other parts of speech)
+#         - Multiple english translations (to meet or to hit / strike)
+#         - unicode control characters in conj table (whyyy)
 
-### 1. Get all dictentry table rows:
+### B. Get all dictentry table rows from verbs section:
 # need parens around the entire regex because we're using findall
 #regex = re.compile(f'(<tr(((?!>).)*)>(((?!</tr>).)*)data-dz-flex-label-1="{ verb }( \([0-9]+\))?"(((?!</tr>).)*)</tr>)')
 regex = re.compile(f'(<tr(((?!>).)*)data-dz-ui="dictentry"(((?!>).)*)>(((?!</tr>).)*)</tr>)')
 # use findall to find all matches even though we're *probably* interested in the 1st one
-rows = regex.findall(verb_search)
+rows = regex.findall(section_dic['Verbs'])
 if len(rows) == 0:
-    print("Verb not found on dict.leo.org!")
+    print("Verb dictionary entries not found on dict.leo.org!")
     exit()
 
-
-### 2. Find table cell with conj table link
-###    and 3. Find table cell with info link
-###    and 4. Find table cell with translation info
-conj_cell = None
-info_cell = None
-trans_cell = None
+# Get English translations
 i = 0
-while not conj_cell and not info_cell and not trans_cell and i < len(rows):
-    # We want these to come from the same row
-    conj_cell = None
-    info_cell = None
-    trans_cell = None
-    # conjugation cell:
-    regex = re.compile(f'<td(((?!>).)*)>(((?!</td>).)*)data-dz-flex-label-1="{ verb }( \([0-9]+\))?"(((?!</td>).)*)"Open verb table"(((?!</td>).)*)</td>')
-    conj_cell = regex.search(rows[i][0])
-    info_cell = re.search(r'<td(((?!>).)*)>(((?!</td>).)*)data-dz-rel-aiid=(((?!</td>).)*)</td>', rows[i][0])
-    trans_cell = re.search(r'<td(((?!>).)*)>(((?!</td>).)*)<a href="/german-english/(((?!</td>).)*)</td>', rows[i][0])
+trans_cell = None
+t_vars["verb_en"] = ""
+while not trans_cell and i < len(rows):
+    # Looking for English translations - it's the first cell with /german-english (and we pull from these) but more
+    # definitively it has:
+    #     <td data-dz-attr="relink" lang="en">
+    # telling us this cell relinks to english words
+    trans_cell = re.search(r'<td data-dz-attr="relink" lang="en">(((?!</td>).)*)</td>', rows[i][0])
+
+    ### Find translation info
+    trans_match = re.findall(r'/german-english/([a-zA-z-]+)', trans_cell.group())
+    trans = ' '.join(trans_match)
+    print(f"English translation found: { trans }")
+    answer = ""
+    while answer.upper() not in ["Y", "N", "Q"]:
+        print("Add / Quit (Y/n/Q)?")
+        answer = sys.stdin.read(1)
+        sys.stdin.readline()
+    if answer.upper() == "Y":
+        if len(t_vars["verb_en"]) > 0:
+            t_vars["verb_en"] += "; "
+        t_vars["verb_en"] += trans
+        print(f"English translation now: { t_vars['verb_en'] }")
+        trans_cell = None
+    if answer.upper() == "N":
+        trans_cell = None
     i += 1
-if not conj_cell or not info_cell or not trans_cell:
-    print("Verb info not found on dict.leo.org!")
-    exit()
-# this gets the link & verb table:
-link = re.search(r'<a href="(((?!").)*)"', conj_cell.group())
-table = requests.get("https://dict.leo.org" + link.groups()[0]).text
-print(link.groups()[0])
-
-# this gets the table key (to link to from our template)
-key = re.search(r'data-dz-flex-table-1="(((?!").)*)"', conj_cell.group())
-t_vars["verb_leo"] = key.groups()[0]
-
-
-### 3. Find table cell with info link
-# <td><a aria-label="Additional information" href="/pages/addinfo/addInfo.php?aiid=Dev5odCDopP&amp;lp=ende&amp;lang=en"><i role="img " title="Additional information" alt="i" class="icon noselect icon_information-outline icon_size_18 darkgray "><dz-data data-dz-ui="dictentry:showInfobox" data-dz-rel-aiid="Dev5odCDopP" data-dz-sprite="false"/> </i></a></td>
-info_key = re.search(r'data-dz-rel-aiid="(((?!").)*)"', info_cell.group())
-t_vars["info_leo"] = info_key.groups()[0]
-
-### 4. Find translation info
-
-trans_match = re.findall(r'/german-english/([a-zA-z-]+)', trans_cell.group())
-t_vars["verb_en"] = ' '.join(trans_match)
-
-# Find verb conjugations (present tense only)
-
-# <span> ich</span><span> h<span class="pink">abe</span></span>
 
 leo_pronouns = {"ich": "<span> ich</span>",
                 "du": "<span> du</span>",
-                "er": '<span title="Singular Maskulin"> er</span><span title="Singular Feminin">/​sie</span><span title="Singular Neutrum">/​es</span>',
+                "er": '<span title="Singular Maskulin"> er</span><span title="Singular Feminin">/sie</span><span title="Singular Neutrum">/es</span>',
                 "wir": "<span> wir</span>",
                 "ihr": "<span> ihr</span>",
                 "sie": "<span> sie</span>"}
-pronouns = {}
-
-for pronoun in leo_pronouns.keys():
-    regex = re.compile(f'{ leo_pronouns[pronoun] }<span> ([a-z]*)<span class="pink">([a-z]+)</span></span>')
-    match = regex.search(table)
-    if match:
-        # Luckily present tense comes first
-        t_vars["conj_" + pronoun] = ''.join(list(match.groups()))
 title = f"{ t_vars['verb_de'] } \u2014 { t_vars['verb_en'] }"
 
+# Get conjugations
+i = 0
+conj_cell = None
+while not conj_cell and i < len(rows):
+    # Looking for conjugation tables
+    regex = re.compile(f'<td(((?!>).)*)>(((?!</td>).)*)data-dz-flex-label-1="{ verb }( \([0-9]+\))?"(((?!</td>).)*)"Open verb table"(((?!</td>).)*)</td>')
+    conj_cell = regex.search(rows[i][0])
+    if conj_cell:
+        # this gets the link & verb table:
+        link = re.search(r'<a href="(((?!").)*)"', conj_cell.group())
+        table = sanitize_text(requests.get("https://dict.leo.org" + link.groups()[0]).text)
+        table = table.replace('<200b>','') # Remove zero width spaces if they exist
+        # this gets the table key (to link to from our template)
+        key = re.search(r'data-dz-flex-table-1="(((?!").)*)"', conj_cell.group())
+        t_vars["verb_leo"] = key.groups()[0]
+
+        print()
+        print('Conjugation:')
+        for pronoun in leo_pronouns.keys():
+            regex = re.compile(f'{ leo_pronouns[pronoun] }<span> ([a-z]*)<span class="pink">([a-z]+)</span></span>')
+            match = regex.search(table)
+            if match:
+                # Luckily present tense comes first
+                t_vars["conj_" + pronoun] = ''.join(list(match.groups()))
+            else:
+                t_vars["conj_" + pronoun] = ""
+            print(f'{ pronoun }: { t_vars["conj_" + pronoun] }')
+        answer = ""
+        while answer.upper() not in ["Y", "N"]:
+            print("Conj OK? (Y/n)")
+            answer = sys.stdin.read(1)
+            sys.stdin.readline()
+            if answer.upper() == "N":
+                conj_cell = None
+    i += 1
+
+# Get Info page
+i = 0
+info_cell = None
+while not info_cell and i < len(rows):
+    info_cell = None
+    info_cell = re.search(r'<td(((?!>).)*)>(((?!</td>).)*)data-dz-rel-aiid=(((?!</td>).)*)</td>', rows[i][0])
+    i += 1
+if not info_cell:
+    print("Information page not found on leo!")
+    exit()
+info_key = re.search(r'data-dz-rel-aiid="(((?!").)*)"', info_cell.group())
+t_vars["info_leo"] = info_key.groups()[0]
+
+print()
 print(json.dumps(t_vars, indent=4))
 print()
 print(f"Title: { title }")
@@ -152,7 +204,7 @@ new_post["categories"] = "Verbs"
 api = "/sites/" + site + "/posts/new/" 
 url = api_url + api
 print(url)
-wp_req = requests.post(url, headers=header, data=new_post)
+wp_req = requests.post(url, headers=auth_header, data=new_post)
 print(wp_req)
 wp_resp = json.loads(wp_req.text)
 print(json.dumps(wp_resp, indent=4))
