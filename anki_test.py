965e6080b0fd839c18028965e466ea588343dbb7
Tarah Tamayo <tarah@tarah.us>
2021-02-10 02:14:44 +0000

    Anki test now creates a note, writes to database (on close), and reopens to verify

--- anki_test.py
+++ anki_test.py
@@ -1,6 +1,12 @@
+# 
+# anki_test.py - Tarah Z. Tamayo
+#    -- Testing out retrieval and creation of collections / decks / cards using the anki library
+# 
+
 import os
 from zipfile import ZipFile
 import anki
+import json
 
 cwd = os.getcwd()
 testfile = cwd + "/data/test.apkg"
@@ -25,3 +31,52 @@ print(card)
 print("\/\/\/\/ NOTE \/\/\/\/")
 note = colln.getNote(card.nid)
 print(note)
+
+print("\nMODELS")
+for model in colln.models.all():
+    print(model)
+    print()
+
+print("\nCREATING A COLLECTION...")
+new_testfile = cwd + "/data/test/collection.anki2"
+new_colln = anki.Collection(new_testfile)
+print(new_colln)
+
+print("\nCREATING A DECK...")
+deck = new_colln.decks.id("Test 1::Test 2::Test 3")
+print(new_colln.decks.all_names_and_ids())
+
+print("\nGET MODELS")
+models = new_colln.models.all()
+# For what I'm doing I'll only want to be using default models "Basic" and "Basic (and reversed card)" - these are built in
+# and easy to retrieve
+for m in models:
+    print(m)
+    print()
+    # We want the Basic & Reversed model
+    if m['name'] == "Basic (and reversed card)":
+        model = m
+
+print("\nCREATING A BASIC NOTE...")
+# Looking at collection.py, this creates a new note with the active model
+# note = new_colln.newNote()
+# Let's choose a model with
+note = anki.notes.Note(new_colln, model)
+note.fields = ['to go', 'gehen']
+print(note)
+
+print("\nADDING NOTE TO DECK...")
+new_colln.add_note(note, deck)
+
+print("\nWRITING THE COLLECTION...")
+new_colln.close()
+
+print("\nREADING THE COLLECTION...")
+new_colln = anki.Collection(new_testfile)
+print(new_colln)
+
+print("\nREADING ALL NOTES...")
+all_notes = new_colln.find_notes("")
+for n in all_notes:
+    print(new_colln.getNote(n))
+    print()
